f59a9522ceebcb18969e60ef112ae4a69532c253
Arvinnli <36981428+Arvinnli@users.noreply.github.com>
2018-04-19 16:23:20 +0800

    1

--- Interview_practice/ip1/tmp.cpp
+++ Interview_practice/ip1/tmp.cpp
@@ -21,7 +21,7 @@ namespace tmp{
     }
 
     //动态规划dynamic programming
-    int getMethod_dp(int step,int mem[]){
+    int getMethod_dp(int step,long long mem[]){
         if(step<0){
             return 0;
         }else if(step==0){
@@ -29,12 +29,33 @@ namespace tmp{
         }else if(mem[step]!=0){
             return mem[step];
         }else{
-            int value = getMethod_dp(step-1,mem)+getMethod_dp(step-1,mem);
+            long long value = getMethod_dp(step-1,mem)+getMethod_dp(step-1,mem);
             mem[step] = value;
             return value;
         }
     }
+
+    public static int sum2(int step, int mem[]) {
+        int i;
+        mem[0] = 1;
+        mem[1] = 1; //为什么要提前初始化这一项？
+        for (i = 2; i <= step; i++) {
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[step];
+    }
+
     void run(int argc,char** argv){
+        long long buf[10*1024] = {0};
+        int n = atoi(argv[1]);
+        getMethod_dp(n,buf);
+
+        
+        // for(int i=0;buf[i]&&i<10*1024;i++){
+        for(int i=0;i<100;i++){
+            cout << "buf[" << i << "]=" << buf[i] << endl;
+        }
+
 
     }
 }
