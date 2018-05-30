1c88ca87979eda516acad8010603fe0f78703424
Arvinnli <36981428+Arvinnli@users.noreply.github.com>
2018-05-30 11:15:43 +0800

    1

--- ComputerSciences/AudioPlayer/test.c
+++ ComputerSciences/AudioPlayer/test.c
@@ -8,9 +8,25 @@
 #include <string.h>
 
 
-
+typedef struct{
+    int size;
+    int a[0];
+}buffer;
 int main(int argc,char** argv){
 
+    int i;
+    buffer buf = {0};
+    buf.size = 100;
+
+    buf.a = malloc(buf.size);
+    for(i=0;i<buf.size;i++){
+        buf.a[i] = i;
+    }
+
+    free(buf.a);
+    buf.size = 0;
+
+    
 
     return 0;
 }
