9dbbc81585959f3af6f6fd3ab6ce3e888c0fa9a1
Arvinnli <36981428+Arvinnli@users.noreply.github.com>
2018-06-21 17:10:36 +0800

    v

--- Demo/drawlight/drawlight.cpp
+++ Demo/drawlight/drawlight.cpp
@@ -13,6 +13,7 @@ typedef struct{
     float emissive;         //光强度
     float reflectivity;     //反射
     float eta;              //折射率
+    float absorption;       //吸收率
 }Result;
 
 #define TWO_PI 6.28318530718f
@@ -34,8 +35,8 @@ float planeSDF(float x,float y,float px,float py,float nx,float ny);
 Result scene(float x,float y);
 void gradient(float x,float y,float* nx,float* ny);
 int refract(float ix,float iy,float nx,float ny,float eta,float* rx,float* ry);
-
-
+float fresnel(float cosi, float cost, float etai, float etat);
+float schlick(float cosi,float cost,float etai,float etat);
 int main(int argc,char** argv){
     char buf[40] = {0};
     // srand((unsigned)time(NULL));
@@ -114,7 +115,9 @@ int refract(float ix, float iy, float nx, float ny, float eta, float* rx, float*
     *ry = eta * iy - a * ny;
     return 1;
 }
-
+float beerLambert(float a, float d) {
+    return expf(-a * d);
+}
 float trace(float ox, float oy, float dx, float dy, int depth) {
     float t = 1e-3f;
     float sign = scene(ox, oy).sd > 0.0f ? 1.0f : -1.0f; // 内还是外？
@@ -127,18 +130,28 @@ float trace(float ox, float oy, float dx, float dy, int depth) {
                 float nx, ny, rx, ry, refl = r.reflectivity;
                 gradient(x, y, &nx, &ny);
                 nx *= sign; ny *= sign; // 在内的话，要反转法线
-                if (r.eta > 0.0f) {
-                    if (refract(dx, dy, nx, ny, sign < 0.0f ? r.eta : 1.0f / r.eta, &rx, &ry))
-                        sum += (1.0f - refl) * trace(x - nx * BIAS, y - ny * BIAS, rx, ry, depth + 1);
-                    else
-                        refl = 1.0f; // 全内反射
+                // if (r.eta > 0.0f) {
+                //     if (refract(dx, dy, nx, ny, sign < 0.0f ? r.eta : 1.0f / r.eta, &rx, &ry))
+                //         sum += (1.0f - refl) * trace(x - nx * BIAS, y - ny * BIAS, rx, ry, depth + 1);
+                //     else
+                //         refl = 1.0f; // 全内反射
+                // }
+                if(r.eta > 0.0f){
+                    if(refract(dx,dy,nx,ny,sign < 0.0f ? r.eta : 1.0f/r.eta,&rx,&ry)){
+                        float cosi = -(dx*nx + dy*ny);
+                        float cost = -(rx*nx + ry*ny);
+                        refl = sign < 0.0f ? fresnel(cosi,cost,r.eta,1.0f) : fresnel(cosi,cost,1.0f,r.eta);
+                        sum += (1.0f - refl)*trace(x - nx*BIAS,y-ny*BIAS,rx,ry,depth+1);
+                    }else{
+                        refl = 1.0f;
+                    }
                 }
                 if (refl > 0.0f) {
                     reflect(dx, dy, nx, ny, &rx, &ry);
                     sum += refl * trace(x + nx * BIAS, y + ny * BIAS, rx, ry, depth + 1);
                 }
             }
-            return sum;
+            return sum*beerLambert(r.absorption,t);
         }
         t += r.sd * sign;
     }
@@ -153,12 +166,18 @@ float sample(float x, float y) {
     }
     return sum / N;
 }
-
+float schlick(float cosi, float cost, float etai, float etat) {
+    float r0 = (etai - etat) / (etai + etat);
+    r0 *= r0;
+    float a = 1.0f - (etai < etat ? cosi : cost);
+    float aa = a * a;
+    return r0 + (1.0f - r0) * aa * aa * a;
+}
 
 Result scene(float x,float y){
   
-    Result a = { circleSDF(x, y, -0.1f, -0.1f, 0.1f), 5.0f, 0.0f, 0.0f };
-    Result b = {    boxSDF(x, y, 0.5f, 0.5f, 0.0f, 0.3, 0.2f), 0.0f, 0.2f, 1.5f};
+    Result a = { circleSDF(x, y, -0.2f, -0.2f, 0.1f), 3.0f, 0.0f, 0.0f, 0.0f };
+    Result b = {    boxSDF(x, y, 0.5f, 0.5f, 0.0f, 0.3, 0.2f), 0.0f, 0.2f, 1.5f, 4.0f };
     return unionOp(a, b);
 
 
@@ -190,6 +209,12 @@ Result scene(float x,float y){
 #endif
 }
 
+
+float fresnel(float cosi, float cost, float etai, float etat) {
+    float rs = (etat * cosi - etai * cost) / (etat * cosi + etai * cost);
+    float rp = (etai * cosi - etat * cost) / (etai * cosi + etat * cost);
+    return (rs * rs + rp * rp) * 0.5f;
+}
 
 
 
---
+++ Demo/hilbert/hilbert.cpp
@@ -0,0 +1,37 @@
+#include <stido.h>
+#include <stdli.h>
+#include <iostream>
+
+using namespace std;
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
