9b2337c3f4fc4bd3ced9acdfcbe7912305941dfd
Arvinn li (李光) <arvinn.li@spreadtrum.com>
2018-08-24 10:17:31 +0800

    1

---
+++ Demo/excel_edit/caron.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+// #include <String>
+#include <iostream>
+#include <string.h>
+
+#ifdef __DEBUG
+#define LOG(...) printf(__VA_ARGS__)
+#else
+#define LOG(...) 
+#endif
+
+
+
+struct Csv{
+    char spc_id[20];
+    char bzid[20];
+    char bzurl[128];
+    char state[20];
+    char headline[796];
+    char owner[20];
+    char contacter[20];
+};
+int Csv_get(const char* s,char* d){
+    int i,j;
+    if(s==NULL||d==NULL||s[0]==0){
+        return -1;
+    }
+    for(i=0;s[i]==',';i++);
+        LOG("%d\n",__LINE__);
+    for(j=0;(s[i+j]!=',')&&(s[i+j]!=0);j++){
+        LOG("%d\n",__LINE__);
+        d[j]=s[i+j];
+        LOG("%d\n",__LINE__);
+        
+    }
+    return i+j;
+}
+int main(int argc,char** argv){
+
+    Csv tmp;
+    char buf[128]={0};
+    char r_buf[1024];
+    // char w_buf[1024];
+    if(argc<2){
+        std::cout << "missing parameters." << std::endl;
+        return 0;
+    }
+    int a=3,b=2,c=1;
+    a=1+(b=c=4);
+    
+    FILE* fp = fopen(argv[1],"r");
+    sprintf(buf,"_n%s",argv[1]);
+        
+    FILE* fp_n = fopen(buf,"w");
+    fgets(r_buf,1023,fp);
+    fputs(r_buf,fp_n);
+    // printf("%s",r_buf);
+    while(!feof(fp)){
+        int i,j,k;
+
+        memset(r_buf,0,sizeof(buf));
+        memset(&tmp,0,sizeof(buf));
+        // memset(w_buf,0,sizeof(buf));
+        fgets((char*)&r_buf,1023,fp);
+        
+
+
+        LOG("%d\n",__LINE__);
+        int n = Csv_get(r_buf,tmp.spc_id);
+        LOG("%d\n",__LINE__);
+        if(n==-1){
+            goto __END;
+        }
+        // printf("%s\n",tmp.spc_id);
+
+
+        n += Csv_get((char*)&r_buf[n],tmp.bzid);
+        // printf("%s\n",tmp.bzid);
+        
+       
+        n += Csv_get((char*)&r_buf[n],tmp.state);
+        // printf("%s\n",tmp.state);
+
+
+        n += Csv_get((char*)&r_buf[n],tmp.headline);
+        // printf("%s\n",tmp.headline);
+        
+        n += Csv_get((char*)&r_buf[n],tmp.owner);
+        // printf("%s\n",tmp.owner);
+        
+        n += Csv_get((char*)&r_buf[n],tmp.contacter);
+        // printf("%s\n",tmp.contacter);
+        
+        sprintf(tmp.bzurl,"=HYPERLINK(\"\"http://bugzilla.unisoc.com/bugzilla/show_bug.cgi?id=%s\"\",\"\"%s\"\")",tmp.bzid,tmp.bzid);
+        for(int i=0;tmp.contacter[i];i++){
+            if(tmp.contacter[i]=='?'||tmp.contacter[i]=='\n'){
+                tmp.contacter[i]=0;
+            }
+        }
+        
+        fprintf(fp_n,"\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"",tmp.spc_id,tmp.bzurl,tmp.state,tmp.headline,tmp.owner,tmp.contacter);
+        fprintf(fp_n,"\n");
+        // printf("%s%s%s%s%s",tmp.spc_id,tmp.bzid,tmp.headline,tmp.owner,tmp.contacter);
+
+        // for(j=0;r_buf[i+j]!=',';j++){
+        //     tmp.bzid[j] = r_buf[i+j];
+        // }
+        // for(k=0;r_buf[i+j+k];k++){
+        //     tmp.rest[k] = r_buf[i+j+k];
+        // }
+                            
+        // fprintf(fp_n,"%s,%s%s",tmp.spc_id,tmp.bzurl,tmp.rest);
+        //std::cout << __LINE__ << std::endl;
+
+    }
+__END:
+    fclose(fp_n);
+    fclose(fp);
+
+    return 0;
+}
--- Demo/tmp.cpp
+++ Demo/tmp.cpp
@@ -1,75 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+template <typename T1,typename T2,typename T3>
+class Test{
+    public:
+        T1 t1;
+        T2 t2;
+        T3 t3;
+    private:
+    protected:
+};
+int main(int argc,char** argv){
+
+    Test<int,char,char*> test;
+
+    test.t1 = 100;
+    test.t2 = 'h';
+    test.t3 = "hello";
+
+    cout << test.t1 << test.t2 << test.t3 << endl;
 
 
-/* NOTE that if CONFIG_FEATURE_USE_INITTAB is NOT defined,
-    * then parse_inittab() simply adds in some default
-    * actions(i.e., runs INIT_SCRIPT and then starts a pair
-    * of "askfirst" shells). If CONFIG_FEATURE_USE_INITTAB
-    * _is_ defined, but /etc/inittab is missing, this
-    * results in the same set of default behaviors.
-    */
-static void parse_inittab(void)
-{
-#if ENABLE_FEATURE_USE_INITTAB
-    char *token[4];
-    parser_t *parser = config_open2("/etc/inittab", fopen_for_read);
-
-    if (parser == NULL)
-#endif
-    {
-        /* No inittab file - set up some default behavior */
-        /* Reboot on Ctrl-Alt-Del */
-        new_init_action(CTRLALTDEL, "reboot", "");
-        /* Umount all filesystems on halt/reboot */
-        new_init_action(SHUTDOWN, "umount -a -r", "");
-        /* Swapoff on halt/reboot */
-        if (ENABLE_SWAPONOFF)
-            new_init_action(SHUTDOWN, "swapoff -a", "");
-        /* Prepare to restart init when a QUIT is received */
-        new_init_action(RESTART, "init", "");
-        /* Askfirst shell on tty1-4 */
-        new_init_action(ASKFIRST, bb_default_login_shell, "");
-//TODO: VC_1 instead of ""? "" is console -> ctty problems -> angry users
-        new_init_action(ASKFIRST, bb_default_login_shell, VC_2);
-        new_init_action(ASKFIRST, bb_default_login_shell, VC_3);
-        new_init_action(ASKFIRST, bb_default_login_shell, VC_4);
-        /* sysinit */
-        new_init_action(SYSINIT, INIT_SCRIPT, "");
-        return;
-    }
-
-#if ENABLE_FEATURE_USE_INITTAB
-    /* optional_tty:ignored_runlevel:action:command
-        * Delims are not to be collapsed and need exactly 4 tokens
-        */
-    while (config_read(parser, token, 4, 0, "#:",
-                PARSE_NORMAL & ~(PARSE_TRIM | PARSE_COLLAPSE))) {
-        /* order must correspond to SYSINIT..RESTART constants */
-        static const char actions[] ALIGN1 =
-            "sysinit\0""wait\0""once\0""respawn\0""askfirst\0"
-            "ctrlaltdel\0""shutdown\0""restart\0";
-        int action;
-        char *tty = token[0];
-
-        if (!token[3]) /* less than 4 tokens */
-            goto bad_entry;
-        action = index_in_strings(actions, token[2]);
-        if (action < 0 || !token[3][0]) /* token[3]: command */
-            goto bad_entry;
-        /* turn .*TTY -> /dev/TTY */
-        if (tty[0]) {
-            if (strncmp(tty, "/dev/", 5) == 0)
-                tty += 5;
-            tty = concat_path_file("/dev/", tty);
-        }
-        new_init_action(1 << action, token[3], tty);
-        if (tty[0])
-            free(tty);
-        continue;
-    bad_entry:
-        message(L_LOG | L_CONSOLE, "Bad inittab entry at line %d",
-                parser->lineno);
-    }
-    config_close(parser);
-#endif
+    return 0;
 }
-
