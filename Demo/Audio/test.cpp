7dbb4513ab2818d24e2bef8801bf5efc4a9b949c
Arvinn li (李光) <arvinn.li@spreadtrum.com>
2018-08-28 10:12:12 +0800

    f

---
+++ Demo/Audio/test.cpp
@@ -0,0 +1,23 @@
+
+#include <windows.h>
+#include <mmsystem.h>
+#include <stdio.h>
+#include <conio.h>
+
+#pragma comment(lib, "winmm.lib")
+
+
+
+int main(int argc,const char* argv[]){
+
+    MMRESULT ret = 0;
+    MSG msg;
+
+    
+
+    
+
+
+
+    return 0;
+}
